aab50ab75a030580184370f630ef43fdf67017b1
Aurélien Matouillot <a.matouillot@gmail.com>
2013-01-09 18:04:13 +0100

    python package

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.0'
+
+setup(name='pyramid_sqladmin/',
+      version=version,
+      description="Simple way to edit your SQLAlchemy objects in pyramid",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='Aur\xc3\xa9lien Matouillot',
+      author_email='a.matouillot@gmail.com',
+      url='https://github.com/LeResKP/pyramid_sqladmin',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
