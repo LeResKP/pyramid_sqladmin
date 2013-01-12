c7cda87ab0f22cedbfdd0a9543d508781cdb04d8
Aurélien Matouillot <a.matouillot@gmail.com>
2013-01-13 00:22:07 +0100

    Fix requires

--- setup.py
+++ setup.py
@@ -1,6 +1,13 @@
 from setuptools import setup, find_packages
 import sys, os
 
+# Hack to prevent TypeError: 'NoneType' object is not callable error
+# on exit of python setup.py test
+try:
+    import multiprocessing
+except ImportError:
+    pass
+
 version = '0.0'
 
 setup(name='pyramid_sqladmin/',
@@ -24,10 +31,15 @@ setup(name='pyramid_sqladmin/',
           'zope.sqlalchemy',
           'tw2.core',
           'tw2.sqla',
+          'mako',
       ],
       test_suite = 'nose.collector',
       tests_require=[
           'nose',
+          'WebTest',
+          'FormEncode',
+          'BeautifulSoup',
+          'strainer',
       ],
       entry_points="""
       # -*- Entry points: -*-
